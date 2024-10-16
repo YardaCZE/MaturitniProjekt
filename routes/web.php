48fb2f515be7bd42ec61756c5c0d002b1ba357d5
Yarda <jarousekpecka@gmail.com>
2024-10-16 07:44:29 +0200

    Přidání aby neregistrovaný uživatel nemohl nikam než na welcom page

--- routes/web.php
+++ routes/web.php
@@ -4,26 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SkupinaController;
 
 
-Route::get('/skupiny', [SkupinaController::class, 'index'])->name('skupiny.index');
-
-// Route pro zobrazení formuláře pro vytvoření skupiny
-Route::get('/skupiny/create', [SkupinaController::class, 'create'])->name('skupiny.create');
-
-
-
-Route::get('/skupiny/prihlasit', function () {
-    return view('skupiny.prihlasit-se');
-})->name('skupiny.prihlasit-se');
-
-Route::post('/skupiny/prihlasit', [SkupinaController::class, 'prihlasit'])->name('skupiny.prihlasit');;
-
-
-// Route pro zobrazení detailu skupiny
-Route::get('/skupiny/{id}', [SkupinaController::class, 'show'])->name('skupiny.show');
-
-Route::post('/skupiny', [SkupinaController::class, 'store'])->name('skupiny.store');
-
-
 
 
 
@@ -43,6 +23,32 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+
+
+
+    Route::get('/skupiny', [SkupinaController::class, 'index'])->name('skupiny.index');
+
+
+    Route::get('/skupiny/create', [SkupinaController::class, 'create'])->name('skupiny.create');
+
+
+
+    Route::get('/skupiny/prihlasit', function () {
+        return view('skupiny.prihlasit-se');
+    })->name('skupiny.prihlasit-se');
+
+    Route::post('/skupiny/prihlasit', [SkupinaController::class, 'prihlasit'])->name('skupiny.prihlasit');;
+
+
+
+    Route::get('/skupiny/{id}', [SkupinaController::class, 'show'])->name('skupiny.show');
+
+    Route::post('/skupiny', [SkupinaController::class, 'store'])->name('skupiny.store');
+
+
+
+
 });
 
 Route::middleware([
