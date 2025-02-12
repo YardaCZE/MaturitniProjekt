ad817c00165f0678653947f30c314d25fa2cf875
YardaCZE <129170792+YardaCZE@users.noreply.github.com>
2025-02-12 15:37:38 +0100

    404 stranka

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <h1 class="text-4xl text-gray-800 text-center mt-12">404 - Stránka nenalezena</h1>
+    <p class="text-lg text-gray-600 text-center mt-4">Omlouváme se, ale stránka, kterou hledáte, neexistuje.</p>
+    <a href="{{ route('dashboard') }}" class="block w-fit mx-auto mt-6 px-6 py-3 bg-blue-500 text-white text-center rounded-lg hover:bg-blue-700 transition-colors">Zpět na dashboard</a>
+
+</x-app-layout>
