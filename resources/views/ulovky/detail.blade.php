ff1660111c0022be59bc4825572cd294de3ab2a5
YardaCZE <129170792+YardaCZE@users.noreply.github.com>
2025-02-18 10:47:27 +0100

    pokus o galerii pomoci flowbite

--- resources/views/ulovky/detail.blade.php
+++ resources/views/ulovky/detail.blade.php
@@ -28,17 +28,51 @@
                 </div>
 
                 @if($ulovek->obrazky->isNotEmpty())
-                    <div class="mt-6">
-                        <h3 class="text-xl font-semibold text-gray-800 mb-4">Obrázky:</h3>
-                        <div class="flex flex-wrap justify-center">
+{{--                    <div class="mt-6">--}}
+{{--                        <h3 class="text-xl font-semibold text-gray-800 mb-4">Obrázky:</h3>--}}
+{{--                        <div class="flex flex-wrap justify-center">--}}
+{{--                            @foreach($ulovek->obrazky as $obrazek)--}}
+{{--                                <div class="m-2 w-1/4 h-32 overflow-hidden rounded-lg shadow cursor-pointer"--}}
+{{--                                     onclick="showModal('{{ asset($obrazek->cesta_k_obrazku) }}')">--}}
+{{--                                    <img src="{{ asset($obrazek->cesta_k_obrazku) }}" alt="Obrázek" class="w-full h-full object-cover">--}}
+{{--                                </div>--}}
+{{--                            @endforeach--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+
+
+
+
+                    <div id="custom-controls-gallery" class="relative w-full" data-carousel="slide">
+                        <!-- Carousel wrapper -->
+                        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                            <!-- Item 1 -->
                             @foreach($ulovek->obrazky as $obrazek)
-                                <div class="m-2 w-1/4 h-32 overflow-hidden rounded-lg shadow cursor-pointer"
-                                     onclick="showModal('{{ asset($obrazek->cesta_k_obrazku) }}')">
-                                    <img src="{{ asset($obrazek->cesta_k_obrazku) }}" alt="Obrázek" class="w-full h-full object-cover">
-                                </div>
+                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                <img src="{{ asset($obrazek->cesta_k_obrazku) }}" class="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="obrazek">
+                            </div>
                             @endforeach
                         </div>
+                        <div class="flex justify-center items-center pt-4">
+                            <button type="button" class="flex justify-center items-center me-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
+            <span class="text-gray-400 hover:text-gray-900 dark:hover:text-white group-focus:text-gray-900 dark:group-focus:text-white">
+                <svg class="rtl:rotate-180 w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
+                </svg>
+                <span class="sr-only">Previous</span>
+            </span>
+                            </button>
+                            <button type="button" class="flex justify-center items-center h-full cursor-pointer group focus:outline-none" data-carousel-next>
+            <span class="text-gray-400 hover:text-gray-900 dark:hover:text-white group-focus:text-gray-900 dark:group-focus:text-white">
+                <svg class="rtl:rotate-180 w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+                <span class="sr-only">Next</span>
+            </span>
+                            </button>
+                        </div>
                     </div>
+
                 @endif
 
                 <!-- Tlačítko pro komentování -->
