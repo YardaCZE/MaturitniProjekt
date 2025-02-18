6bcb76e883d248c1d62f1026c283b6140e5c1aed
YardaCZE <129170792+YardaCZE@users.noreply.github.com>
2025-02-18 14:05:03 +0100

    .

--- resources/views/ulovky/detail.blade.php
+++ resources/views/ulovky/detail.blade.php
@@ -42,7 +42,7 @@
                     <div id="custom-controls-gallery" class="relative w-full" data-carousel="slide">
                         <!-- Carousel wrapper -->
                         <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-                            <!-- Item 1 -->
+                            <!-- Item 1 skibidi -->
                             @foreach($ulovek->obrazky as $obrazek)
                             <div class="hidden duration-700 ease-in-out" data-carousel-item>
                                 <img src="{{ asset($obrazek->cesta_k_obrazku) }}" class="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="obrazek">
