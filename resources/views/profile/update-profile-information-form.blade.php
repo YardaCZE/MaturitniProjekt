98f6eb1a7bc831cfaf776ec292277fcb23368fed
YardaCZE <129170792+YardaCZE@users.noreply.github.com>
2025-02-26 09:03:17 +0100

    update profile info preklad

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -1,10 +1,10 @@
 <x-form-section submit="updateProfileInformation">
     <x-slot name="title">
-        {{ __('Profile Information') }}
+        {{ __('Informace o profilu') }}
     </x-slot>
 
     <x-slot name="description">
-        {{ __('Update your account\'s profile information and email address.') }}
+        {{ __('změntě si údaje o účtu') }}
     </x-slot>
 
     <x-slot name="form">
